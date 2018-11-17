71841bcb2a054a23eb2fc0f9beff611be92fae9a
Iain R. Learmonth <irl@fsfe.org>
2018-11-17 00:48:06 +0000

    Remove redundant tests (also they take a while)

--- bushel/test_downloader.py
+++ bushel/test_downloader.py
@@ -37,30 +37,6 @@ class TestDirectoryDownloader:
             assert isinstance(consensus, NetworkStatusDocumentV3)
         asyncio.run(go(self))
 
-    def test_fetch_all_relay_descriptors(self):
-        if not self.has_local_directory_cache:
-            raise nose.SkipTest(
-                f"No local directory cache available at {LOCAL_DIRECTORY_CACHE}")
-        async def go(self):
-            descriptors = await self.downloader.descriptor(
-                SERVER_DESCRIPTOR,
-                endpoint=LOCAL_DIRECTORY_CACHE)
-            for descriptor in descriptors:
-                assert isinstance(descriptor, RelayDescriptor)
-        asyncio.run(go(self))
-
-    def test_fetch_all_extra_info_descriptors(self):
-        if not self.has_local_directory_cache:
-            raise nose.SkipTest(
-                f"No local directory cache available at {LOCAL_DIRECTORY_CACHE}")
-        async def go(self):
-            descriptors = await self.downloader.descriptor(
-                EXTRA_INFO_DESCRIPTOR,
-                endpoint=LOCAL_DIRECTORY_CACHE)
-            for descriptor in descriptors:
-                assert isinstance(descriptor, RelayExtraInfoDescriptor)
-        asyncio.run(go(self))
-
 def test_url_for():
     cases = {
         (SERVER_DESCRIPTOR, None, None): "/tor/server/all",
